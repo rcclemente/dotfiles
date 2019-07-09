66027ca16a0a371e8bdc5fdbb25dc74bbb0eb086
Ryan Clemente <rclemente@zendesk.com>
2019-07-09 13:55:09 +0800

    update bash_helper for fzf

--- bash_helpers.sh
+++ bash_helpers.sh
@@ -1,28 +1,44 @@
 # my prompt
 export PS1='\n\[\e[34m\][= \[\e[m\]\u@mbp \[\e[32m\]<\w> \[\e[m\]\[\e[30;43m\]$(__git_ps1)\[\e[m\]\[\e[34m\] =\[\e[m\]\[\e[34m\]]\[\e[m\]\[\e[31m\]\n\\$\[\e[m\]\[\e[31m\]>\[\e[m\] '
 
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
-
-alias docker-cleanup='docker rm -v $(docker ps -a -q -f status=exited)'
-alias docker-rmi-cleanup='docker rmi $(docker images -q -f dangling=true)'
-
-lias ll='ls -la'
+# aliases
+alias ll='ls -la'
 alias so='source '
 alias tmux='tmux '
 alias tmux-new='tmux new -s '
 alias ctags-cwd='ctags --languages=ruby,javascript --exclude=.git --exclude=log --recurse . `bundle show --paths`'
-alias vnotes='cd ~/vnotes && vi '
+alias vnotes='cd ~/vnotes && nvim '
 alias dotfiles='cd ~/dotfiles && vi '
+# zdi
+alias zdi-all-restart='zdi services restart && zdi apps restart'
 
-# FZF
+# fzf
 alias gcob='git checkout  $(git branch  | fzf)'
 alias gcobr='git checkout --track $(git branch -r | fzf)'
-alias fzd='cd $(ls -d ~/Code/zendesk/*/ | fzf)'
+alias fzd='cd $(ls -d ~/code/zendesk/*/ | fzf)'
+alias git-show-latesttag='git log --pretty=format:"%d" | grep "tag:" | head -n 1'
 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export fzf_default_opts='--bind=ctrl-n:up,ctrl-p:down'
 
-# VIM
-if [ -n $TMUX ]; then
-   alias vim="TERM=screen-256color vim"
-   alias vi="TERM=screen-256color vim"
+# git
+alias gcoammend='git commit --amend'
+# alias gdlm='git branch --merged master | grep -v '^[ * ]*master$' | xargs git branch -d'
+
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+[ -s "$nvm_dir/bash_completion" ] && \. "$nvm_dir/bash_completion"  # this loads nvm bash_completion
+
+if [ -n $tmux ]; then
+   alias vim="term=screen-256color vim"
+   alias vi="term=screen-256color vim"
 fi
+
+function rollbar-update-ip {
+  sudo sh -c 'for ip in $(dig +short rollbar-eu.zendesk.com | grep -e "^(52|54)"); do route add ${ip} -netmask 255.192.0.0 -iface gpd0; done'
+  sudo sh -c 'for ip in $(dig +short rollbar-us.zendesk.com | grep -e "^(52|54)"); do route add ${ip} -netmask 255.192.0.0 -iface gpd0; done'
+}
+
+function git-prune-local {
+  git checkout master
+  git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d
+}
