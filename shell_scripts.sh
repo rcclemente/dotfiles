7b5f6758a337f4c07ae9781f6df0e1031424d741
ryan clemente <ryanclemente@ryans-MacBook-Pro-14.local>
2024-05-29 20:36:33 +0800

    update

--- shell_scripts.sh
+++ shell_scripts.sh
@@ -86,10 +86,14 @@ function kdevictedpod {
 alias rake='noglob rake'
 
 #java switch
-export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
-export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
-
-alias java8='export JAVA_HOME=$JAVA_8_HOME'
-alias java11='export JAVA_HOME=$JAVA_11_HOME'
+# export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+# export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
+#
+# alias java8='export JAVA_HOME=$JAVA_8_HOME'
+# alias java11='export JAVA_HOME=$JAVA_11_HOME'
 
 alias lvi='nvim -u $HOME/dotfiles/nvim/new_init.lua'
+
+# lazyvim
+alias lzvim="NVIM_APPNAME=lazyvim nvim"
+
